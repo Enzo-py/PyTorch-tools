202d46216996cf28a9328f6b589b61feca0bb9e7
Enzo Pinchon <85191552+Enzo-py@users.noreply.github.com>
2025-09-17 13:14:12 +0200

    update: Add new metrics and update existing ones

--- metrics.py
+++ metrics.py
@@ -33,10 +33,13 @@ class MetricRegistry:
 METRICS = MetricRegistry()
 
 class MetricName(str, Enum):
-    ACC_001 = "Acc<0.01"
+    ACC_001_2T = "Acc<0.01 2T"
     ACC_1 = "Acc<1.0"
+    ACC_001 = "Acc<0.01"
     KL = "KL loss"
     RECST = "Recst. loss"
+    SIMCLR = "SimCLR loss"
+    DWST = "Downstream task loss"
     MU_MEAN = "μ.mean"
     MU_STD = "μ.std"
     LOGVAR_MEAN = "logσ².mean"
@@ -44,14 +47,29 @@ class MetricName(str, Enum):
 
 
 # --- metrics code
+@METRICS.register(MetricName.ACC_001_2T.value, "|x - recon(x)| < 0.01 (avg over towers)")
+def acc_001_twotower(out, target, others):
+    def acc_per_pair(x, x_recon):
+        return 
 
-@METRICS.register(MetricName.ACC_001.value, "|x - y| < 0.01")
-def acc_001(out, target, others):
-    return (torch.abs(out - target) < 0.01).float().mean().item()
+    x1, x2, _ = target
+    x1 = x1[..., (0, 2, 3)]
+    x1_recon = others["model"]["x1"]["recon"]
+    x2_recon = others["model"]["x2"]["recon"]
+
+    acc1 = (torch.abs(x1 - x1_recon) < 0.01).float().mean()
+    acc2 = (torch.abs(x2 - x2_recon) < 0.01).float().mean()
+    return ((acc1 + acc2) / 2).item()
+
 
 @METRICS.register(MetricName.ACC_1.value, "|x - y| < 1.0")
 def acc_1(out, target, others):
     return (torch.abs(out - target) < 1.0).float().mean().item()
+
+@METRICS.register(MetricName.ACC_001.value, "|x - y| < 0.01")
+def acc_001(out, target, others):
+    if isinstance(target, (list, tuple)): target = target[-1]
+    return (torch.abs(out - target) < 0.01).float().mean().item()
 
 @METRICS.register(MetricName.KL.value, "KL loss for VAE")
 def kl(out, target, others):
@@ -64,6 +82,18 @@ def rec(out, target, others):
     if 'REC' in others['loss']:
         return others['loss']['REC']
     else: raise KeyError(f"Metric <{MetricName.RECST.value}> need a loss that return the REC sub loss.")
+
+@METRICS.register(MetricName.SIMCLR.value, "SIMCLR loss for VAE")
+def simclr(out, target, others):
+    if 'SIMCLR' in others['loss']:
+        return others['loss']['SIMCLR']
+    else: raise KeyError(f"Metric <{MetricName.SIMCLR.value}> need a loss that return the SIMCLR sub loss.")
+
+@METRICS.register(MetricName.DWST.value, "Downstream task loss for VAE")
+def simclr(out, target, others):
+    if 'DWST' in others['loss']:
+        return others['loss']['DWST']
+    else: raise KeyError(f"Metric <{MetricName.DWST.value}> need a loss that return the DWST sub loss.")
 
 @METRICS.register(MetricName.MU_MEAN.value, "Mean of μ")
 def mu_mean(out, target, others):
